2080d87b5794d933bace57db19405abc5b3f06df
CaptainFreak <patelshoeb4@gmail.com>
2018-10-28 07:21:40 +0530

    deleting 1

--- s.sh
+++ s.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Basic while loop
 counter=1
-while [ $counter -le 10 ]
+while [ $counter -le 100 ]
 do
 touch flag$counter
+echo "nothing" > flag$counter
 git add .
 git commit -m "deleting $counter"
 done
